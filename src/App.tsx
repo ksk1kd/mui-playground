40d816c91094ccd7052bf5e5f6fc21aef550cb04
Keisuke Ikeda <ksk1kd.dev@gmail.com>
2025-03-04 23:16:54 +0900

    add button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import { Button } from '@mui/material'
+
 function App() {
   return (
     <>
-      <div className="container">Contents</div>
+      <div className="container">
+        <section>
+          <h1>Button</h1>
+          <Button variant="contained">Hello world</Button>
+        </section>
+      </div>
     </>
   )
 }
