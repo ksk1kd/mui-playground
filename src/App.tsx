ede5a7e711f46809915eec2d622b6928de47cbdf
Keisuke Ikeda <ksk1kd.dev@gmail.com>
2025-03-04 23:36:49 +0900

    add text field

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
-import { Button } from '@mui/material'
+import { Button, TextField } from '@mui/material'
+import { useState } from 'react'
 
 function App() {
+  const [input, setInput] = useState('')
+
   return (
     <>
       <div className="container">
+        <section>
+          <h1>Text Field</h1>
+          <TextField
+            id="outlined-basic"
+            label="Outlined"
+            variant="outlined"
+            margin="normal"
+            onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+              setInput(event.target.value)
+            }}
+          />
+          <p>{input}</p>
+        </section>
+
         <section>
           <h1>Button</h1>
           <Button variant="contained">Hello world</Button>
